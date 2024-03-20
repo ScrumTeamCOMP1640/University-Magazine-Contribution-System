b4e9f52c6453ccd881390bb6ae50f8d02050205d
Hiepdv2003 <hiepdvgcs210944@fpt.edu.vn>
2024-03-20 16:15:59 +0700

    Update the frontend

---
+++ source/repos/COMP1640/COMP1640/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP1640.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/User")]
+    public class UserController : Controller
+    {        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [Route("Edit")]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ source/repos/COMP1640/COMP1640/wwwroot/login/js/script.js
@@ -0,0 +1,8 @@
+const showPassword = document.getElementById("show-password");
+const passwordField = document.getElementById("password");
+
+showPassword.addEventListener("click", function() {
+    this.classList.toggle("fa-eye");
+    const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
+    passwordField.setAttribute("type", type);
+})
