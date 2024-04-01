c69b37631dc6063ea00a5b67bf874ae829947ff9
Cdcod <thaiphgcs210953@fpt.edu.vn>
2024-04-01 16:16:35 +0700

    Delete source/repos/COMP1640 directory

--- source/repos/COMP1640/COMP1640/Interfaces/IEmail.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace COMP1640.Interfaces
-{
-    public interface IEmail
-    {
-        Task SendEmailAsync(string email, string subject, string message);
-    }
-}
--- source/repos/COMP1640/COMP1640/Interfaces/IFile.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace COMP1640.Interfaces
-{
-    public interface IFile
-    {
-        Task<bool> UploadFile (IFormFile file);
-
-        Task<(byte[], string, string)> DownloadFile (string fileName);
-    }
-}
