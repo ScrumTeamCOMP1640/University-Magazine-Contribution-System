5d550413cf1e744a0d9331bcc838c861d94d2c83
Cdcod <thaiphgcs210953@fpt.edu.vn>
2024-04-01 15:41:07 +0700

    Delete source/repos/COMP1640 directory

--- source/repos/COMP1640/COMP1640/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace COMP1640.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
--- source/repos/COMP1640/COMP1640/wwwroot/login/js/script.js
+++
@@ -1,8 +0,0 @@
-const showPassword = document.getElementById("show-password");
-const passwordField = document.getElementById("password");
-
-showPassword.addEventListener("click", function() {
-    this.classList.toggle("fa-eye");
-    const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
-    passwordField.setAttribute("type", type);
-})
